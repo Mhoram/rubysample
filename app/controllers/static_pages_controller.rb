9a0bdf3d204b2f049f4c76ebcd62e6a4b461531b
Karl Wallace <karlwallace@gmail.com>
2017-07-23 17:35:52 +0000

    Adding helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,5 +17,15 @@ class StaticPagesController < ApplicationController
       return false
     end
   end
+  
+    # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
     
 end
