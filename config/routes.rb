cb69f9e5552542d1f27b29ddaba5966e58ecb606
Karl Wallace <karlwallace@gmail.com>
2017-07-24 12:20:34 +0000

    API connection

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'consume/list'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get  '/home', to: 'static_pages#home'
