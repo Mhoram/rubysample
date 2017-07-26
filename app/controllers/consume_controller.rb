65b7b9c1adb4bb0fb042b2d0056b5c7e431bb554
Karl Wallace <karlwallace@gmail.com>
2017-07-26 15:42:10 +0000

    Adding scaffolding for statuses and connecting to Twillio API

--- app/controllers/consume_controller.rb
+++ app/controllers/consume_controller.rb
@@ -1,19 +1,30 @@
 class ConsumeController < ApplicationController
   require 'rubygems'
   require 'rest_client'
+  require 'twilio-ruby'
 
   def list
     
     @greeting = "Dia Dhuit"
     
-    #@res = RestClient.get 'https://api.discogs.com/releases/249504'
     @res = RestClient.get 'https://jsonplaceholder.typicode.com/posts'
-   
-    #@res = RestClient.get 'https://api.discogs.com/', :releases => '249504'
-    #@res = RestClient.get("http://en.wikipedia.org/wiki")
-    @res.code                    # => 200
-    @res.headers[:content_type]  # => 'image/jpg'
+    @res.code                   
+    @res.headers[:content_type] 
     @data = JSON.parse(@res.body)
     
+    # put your own credentials here
+    account_sid = 'ACe71618d0c3d35737520ef424047a1f41'
+    auth_token = '338e0bfc2c032a89f4cb74553a7aaba8'
+
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+    
+    @message = @client.messages.create(
+      from: '+353861802780',
+      to: '+353877963817',
+      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+      status_callback: request.base_url + '/message_statuses'
+    )
+  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  resources :message_statuses
+  
   get 'consume/list'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
